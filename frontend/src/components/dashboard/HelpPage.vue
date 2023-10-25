d5df90a5337a160d48971a54af1d78068831d992
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-10-24 23:43:42 -0300

    feat: add help page function

--- frontend/src/components/dashboard/HelpPage.vue
+++ frontend/src/components/dashboard/HelpPage.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="help-list">
-    <div v-for="(topic, index) in topics" :key="index" class="help">
-      <span class="help__title">{{ topic.question }}</span>
-      <img
-        src="@/assets/images/icons/expand.png"
-        alt="See Answer"
-        class="help__icon"
-      />
+    <div
+      v-for="(topic, index) in topics"
+      :key="index"
+      class="help"
+      :ref="`help-card-${index}`"
+    >
+      <div class="help-header">
+        <span class="help__title">{{ topic.question }}</span>
+        <img
+          src="@/assets/images/icons/expand.png"
+          alt="See Answer"
+          class="help__icon expand"
+          @click="toggleHelp(index)"
+        />
+        <img
+          src="@/assets/images/icons/close.png"
+          alt="See Answer"
+          class="help__icon close"
+          @click="toggleHelp(index)"
+        />
+      </div>
+      <div class="help-answer">
+        <span class="help__title">{{ topic.answer }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +67,12 @@ export default {
       ],
     };
   },
+  methods: {
+    toggleHelp(index) {
+      const helpCard = this.$refs[`help-card-${index}`][0];
+      helpCard.classList.toggle("open");
+    },
+  },
 };
 </script>
 
@@ -68,14 +91,12 @@ export default {
   }
   .help {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
     gap: 11px;
     padding: 11px 40px 10px 73px;
     border-radius: 15px;
     background-color: var(--color-background-cards);
     box-shadow: 1px 1px 20px 1px rgba(0, 0, 0, 0.09);
-    min-height: 131px;
     .help__title {
       font-size: 30px;
       font-style: normal;
@@ -86,6 +107,36 @@ export default {
     .help__icon {
       width: 24px;
       height: 24px;
+      cursor: pointer;
+    }
+    .help-header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 35px 0;
+    }
+    .help-answer {
+      display: none;
+    }
+    .expand {
+      display: block;
+    }
+    .close {
+      display: none;
+    }
+    &.open {
+      .help-answer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0 0 35px;
+      }
+      .expand {
+        display: none;
+      }
+      .close {
+        display: block;
+      }
     }
   }
 }
