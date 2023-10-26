ae386c0a00b937dcc18716a95bfeedbc3a5a02bc
kaioalex <caioalex2017@gmail.com>
2023-10-26 01:27:55 -0300

    Added initial route to persist user data to mysql db

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,7 +3,17 @@ from src.configuration import configure_inject
 from src.web.pedido.pedido import pedidos
 from src.web.telas.tela_inicial import telas
 
+from flaskext.mysql import MySQL
+from src.web.telas.tela_inicial import mysql
+
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Root@2023'
+app.config['MYSQL_DATABASE_DB'] = 'test'
+mysql.init_app(app)
+
 configure_inject(app)
 app.register_blueprint(pedidos())
 app.register_blueprint(telas())
--- backend/src/web/telas/tela_inicial.py
+++ backend/src/web/telas/tela_inicial.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, jsonify, Response, request, render_template
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
 
 def telas() -> Blueprint:
     telas_blueprint = Blueprint('telas', __name__)
@@ -6,5 +9,34 @@ def telas() -> Blueprint:
     @telas_blueprint.route('/')
     def tela_inicial():
         return render_template('index.html')
+
+    @telas_blueprint.route('/form')
+    def tela_form():
+        return render_template('form.html')
+
+    @telas_blueprint.route('/login', methods = ['POST', 'GET'])
+    def login():
+        if request.method == 'GET':
+            return "Login via the login Form"
+     
+        if request.method == 'POST':
+            name = request.form['name']
+            username = request.form['username']
+            email = request.form['email']
+            birth = request.form['birth']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+            # cursor.execute("SELECT * from users")
+            # data = cursor.fetchall()
+            # print(data)
+
+            cursor.execute(''' INSERT INTO users (name, username, email, birth) VALUES(%s,%s,%s,%s)''',(name, username, email, birth))
+            conn.commit()
+
+            cursor.close()
+
+            return f"Done!!"
 
     return telas_blueprint
