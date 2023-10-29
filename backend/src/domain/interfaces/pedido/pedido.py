a40e9b49499ca500f6540c73b22282aca553dd66
orudda <othavio.rudda.cc@gmail.com>
2023-10-29 19:11:51 -0300

    refactoring useless code

--- backend/src/domain/interfaces/pedido/pedido.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json, LetterCase
-
-# Documentation: https://docs.python.org/3/library/dataclasses.html#dataclasses.dataclass
-@dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass(init=True)
-class Pedido:
-    id: int
-    title: str
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from src.configuration import configure_inject
 from src.configuration import connect_to_db
-from src.web.pedido.pedido import pedidos
 
 from src.web.friendship.friendship import friendships
 from src.web.user.user import user
@@ -9,7 +8,6 @@ from src.web.user.user import user
 app = Flask(__name__)
 #mysql.init_app(app)
 configure_inject(app)
-app.register_blueprint(pedidos())
 app.register_blueprint(user())
 app.register_blueprint(friendships())
 
