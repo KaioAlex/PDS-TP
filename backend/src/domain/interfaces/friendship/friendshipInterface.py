57f80c65b8a4925e40440b0dec2b8423aabeed0d
orudda <othavio.rudda.cc@gmail.com>
2023-10-29 17:40:38 -0300

    post friendship working

--- backend/src/domain/interfaces/friendship/friendshipInterface.py
+++ backend/src/domain/interfaces/friendship/friendshipInterface.py
@@ -1,12 +1,23 @@
 import inject
 from abc import ABC, abstractmethod
 from typing import List
+from src.configuration import get_cursor
+from src.configuration import get_conn
 
 from src.domain.interfaces.friendship.friendship import Friendship
 
 class FriendshipInterface(ABC):
     def getFriendshipsList(self) -> List[Friendship]:
         pass
+    
+    def post_friendship(self, friendship: Friendship):
+        conn = get_conn()
+        cursor = get_cursor()
 
-    def post_friendship(self, friendship: Friendship) -> List[Friendship]:
-        pass
+        # Faz o post no banco
+        cursor.execute(''' INSERT INTO friendship (user_id1, user_id2) VALUES(%s,%s)''',(friendship.user_id1, friendship.user_id2))
+        conn.commit()
+        
+        cursor.close()
+
+        return 
