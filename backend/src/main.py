51eb77f25c8f0538bef653a89959fd2edc959cfe
kaioalex <caioalex2017@gmail.com>
2023-10-29 18:58:58 -0300

    Added route: 5) Post card info

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,14 +3,13 @@ from src.configuration import configure_inject
 from src.configuration import connect_to_db
 from src.web.pedido.pedido import pedidos
 from src.web.user.user import user
-#from src.web.telas.tela_inicial import telas
+from src.web.card.card import card
 
-#from src.web.telas.tela_inicial import mysql
 app = Flask(__name__)
-#mysql.init_app(app)
+
 configure_inject(app)
 app.register_blueprint(pedidos())
 app.register_blueprint(user())
-#app.register_blueprint(telas())
+app.register_blueprint(card())
 
 connect_to_db()
