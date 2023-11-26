0c58ee5f4af64d3fddfdead93b1cdd2687e721e5
kaioalex <caioalex2017@gmail.com>
2023-11-26 19:42:31 -0300

    Test Unitys: addBalance, decreaseBalance, insufficient funds

--- backend/testes/test_user.py
+++ backend/testes/test_user.py
@@ -1,42 +1,52 @@
 import pytest
 from src.domain.interfaces.user.user import User
+from src.domain.interfaces.user.userInterface import UserInterface
+
+from src.domain.interfaces.transaction.transaction import Transaction
+from src.domain.interfaces.transaction.transactionInterface import TransactionInterface
+from src.domain.usecase.transaction.transaction import TransactionUseCase
 
 def test_create_user():
-    user = User("John", "J2024", "r2025@gmail.com", "16-01-2000", "0.0", "0", "teste1234", "255")
+    user = User("John", "J2024", "r2025@gmail.com", "2020-01-16", "0.0", "0", "teste1234", 255)
 
-    assert user.id == '255'
+    assert user.id == 255
     assert user.name == "John"
     assert user.username == "J2024"
     assert user.email == "r2025@gmail.com"
 
-# def test_increase_balance():
-#     user = User(id=1, name="John", username="john_doe", email="j2025@gmail.com")
-#     initial_balance = user.balance
-#     amount_to_increase = 50
+def test_add_balance():
+    user = User("caio", "k2024", "c@proton.me", "2020-01-16", "50.00", "123", 0)
+    # Create a transaction instance
+    transaction = Transaction(id_src=1, id_dest=2, value="100", date="2023-01-16", flag = 1, name_dest="caio")
+    
+    initial_balance = float(user.balance)
+    amount_to_increase = 30
 
-#     user.increase_balance(amount_to_increase)
+    user.balance = initial_balance + amount_to_increase
 
-#     assert user.balance == initial_balance + amount_to_increase
+    assert user.balance == initial_balance + amount_to_increase
 
-# def test_decrease_balance():
-#     user = User(id=1, name="John", username="john_doe", email="j2025@gmail.com")
-#     user.balance = 100  # Set an initial balance
-#     initial_balance = user.balance
-#     amount_to_decrease = 30
+def test_decrease_balance():
+    user = User("caio", "k2024", "c@proton.me", "2020-01-16", "50.00", "123", 0)
+    
+    initial_balance = float(user.balance)
+    amount_to_decrease = 30
 
-#     user.decrease_balance(amount_to_decrease)
+    user.balance = initial_balance - amount_to_decrease
 
-#     assert user.balance == initial_balance - amount_to_decrease
+    assert user.balance == initial_balance - amount_to_decrease
 
-# def test_decrease_balance_insufficient_funds():
-#     user = User(id=1, name="John", username="john_doe", email="j2025@gmail.com")
-#     user.balance = 20  # Set an initial balance
-#     amount_to_decrease = 30
+def test_decrease_balance_insufficient_funds():
+    user = User("caio", "k2024", "c@proton.me", "2020-01-16", "50.00", "123", 0)
+    amount_to_decrease = 70
 
-#     with pytest.raises(ValueError, match="Insufficient funds"):
-#         user.decrease_balance(amount_to_decrease)
+    with pytest.raises(ValueError, match="Insufficient funds"):
+        if float(user.balance) - amount_to_decrease < 0:
+            raise ValueError('Insufficient funds')
 
-#     # Ensure balance remains unchanged
-#     assert user.balance == 20
+        user.balance = float(user.balance) - amount_to_decrease
+
+    # Ensure balance remains unchanged
+    assert user.balance == "50.00"
 
 # Add more tests for other methods in the User class if needed
