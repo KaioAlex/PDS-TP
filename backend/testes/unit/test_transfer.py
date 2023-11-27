5b9c7b188c6208f12467e0cd99d7809af9d977c9
kaioalex <caioalex2017@gmail.com>
2023-11-27 15:42:51 -0300

    Fix/Fix/Fix test_transfer.py

--- backend/testes/unit/test_transfer.py
+++ backend/testes/unit/test_transfer.py
@@ -10,7 +10,7 @@ def mock_transaction_interface():
 def test_post_transaction_high_value(mock_transaction_interface):
     transactions = Transactions(transactioninterface=mock_transaction_interface)
 
-    high_value_transaction = Transaction(id_src=1, id_dest=2, value="20000", date="2023-01-16", flag = 1, name_dest="caio")
+    high_value_transaction = Transaction(id_src=1, id_dest=2, value=20000.00, date="2023-01-16", flag = 1, name_dest="caio")
 
     with pytest.raises(ValueError, match="valor de transacao muito alto") as err:
         transactions.postTransaction(high_value_transaction)
@@ -20,7 +20,7 @@ def test_post_transaction_high_value(mock_transaction_interface):
 def test_post_transaction_user_high_value(mock_transaction_interface):
     transactions = Transactions(transactioninterface=mock_transaction_interface)
 
-    high_value_transaction = Transaction(id_src=1, id_dest=2, value="20000", date="2023-01-16", flag = 1, name_dest="caio")
+    high_value_transaction = Transaction(id_src=1, id_dest=2, value=20000.00, date="2023-01-16", flag = 1, name_dest="caio")
 
     with pytest.raises(ValueError, match="valor de transacao muito alto") as err:
         transactions.makeUsersTransfer(high_value_transaction)
