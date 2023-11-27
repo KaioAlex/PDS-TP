ee6d3a46dc542220fe08a09838cf863525b8ce10
Othávio Ruddá da Cunha Araújo <48054798+orudda@users.noreply.github.com>
2023-11-27 16:34:55 -0300

    Update friendships.py

--- backend/src/domain/actions/friendship/friendships.py
+++ backend/src/domain/actions/friendship/friendships.py
@@ -14,11 +14,13 @@ class Friendships:
     
     def postFriendship(self, friendship: Friendship) -> List[Friendship]:
         if friendship.user_id1 < 0 or friendship.user_id2 < 0:
+            
             raise ValueError("valor invalido")
         return self.__friendshipinterface.postFriendship(friendship)
     
     def deleteFriendships(self, user_id: int,friend_id: int) -> str:
         if user_id < 0 or friend_id < 0:
+            
             raise ValueError("valor invalido")
         return self.__friendshipinterface.deleteFriendship(user_id, friend_id)
         
