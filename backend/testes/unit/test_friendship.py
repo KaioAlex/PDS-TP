e15fc6fa3490343d0a4d57b4ea34393c6433ae47
orudda <othavio.rudda.cc@gmail.com>
2023-11-27 15:42:36 -0300

    friendship test

--- backend/testes/unit/test_friendship.py
+++ backend/testes/unit/test_friendship.py
@@ -1,28 +1,28 @@
-# import pytest
-# from unittest.mock import MagicMock
-# from src.domain.actions.friendship.friendships import Friendships, FriendshipError
-# from src.domain.interfaces.transaction.transaction import Transaction
+import pytest
+from unittest.mock import MagicMock
+from src.domain.actions.friendship.friendships import Friendships
+from src.domain.interfaces.friendship.friendship import Friendship
 
-# @pytest.fixture
-# def mock_transaction_interface():
-#     return MagicMock()
+@pytest.fixture
+def mock_friendship_interface():
+    return MagicMock()
 
-# def test_post_transaction_high_value(mock_transaction_interface):
-#     friendship = Friendships(transactioninterface=mock_transaction_interface)
+def test_post_friendship_value(mock_friendship_interface):
+    friendship = Friendships(friendshipinterface=mock_friendship_interface)
 
-#     high_value_transaction = Transaction(value="20000")
+    friendship_value_test = Friendship(user_id1 = 0, user_id2 = -1)
 
-#     with pytest.raises(ValorTransacaoAltoError, match="valor de transacao muito alto") as err:
-#         transactions.postTransaction(high_value_transaction)
+    with pytest.raises(ValueError, match="valor invalido") as err:
+        friendship.postFriendship(friendship_value_test)
 
-#     assert str(err.value) == "valor de transacao muito alto"
+    assert str(err.value) == "valor invalido"
 
-# def test_post_transaction_user_high_value(mock_transaction_interface):
-#     transactions = Transactions(transactioninterface=mock_transaction_interface)
+def test_delete_friendship_value(mock_friendship_interface):
+    friendship = Friendships(friendshipinterface=mock_friendship_interface)
 
-#     high_value_transaction = Transaction(value="20000")
+    friendship_value_test = Friendship(user_id1 = 1, user_id2 = -1)
 
-#     with pytest.raises(ValorTransacaoAltoError, match="valor de transacao muito alto") as err:
-#         transactions.makeUsersTransfer(high_value_transaction)
+    with pytest.raises(ValueError, match="valor invalido") as err:
+        friendship.deleteFriendships(friendship_value_test.user_id1, friendship_value_test.user_id2)
 
-#     assert str(err.value) == "valor de transacao muito alto"
+    assert str(err.value) == "valor invalido"
